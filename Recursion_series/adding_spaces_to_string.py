1402454d95f736651d892d5575940a369911033a
Rhitu Thapa <72629071+ThapaRh@users.noreply.github.com>
2022-12-05 10:54:29 -0600

    Create adding_spaces_to_string.py

---
+++ Recursion_series/adding_spaces_to_string.py
@@ -0,0 +1,23 @@
+#This is easy question, trick is if we use string concatenation it gives Time limit exceeded error becasue they say + takes a lot of time 
+
+
+class Solution(object):
+    def addSpaces(self, s, spaces):
+        # new_word = ""
+        # j=0
+        # for i in spaces:
+        #     new_word+=s[j:i]+" "
+        #     j=i
+        # if j<len(s):
+        #     new_word+=s[j:]
+        # return new_word
+        new_word = []
+        sp = set(spaces)
+        for i,c in enumerate(s):
+            if i in sp:
+                new_word.append(" ")
+            new_word.append(c)
+        return "".join(new_word)
+
+newobj = Solution()
+print(newobj.addSpaces("HelloWorld",[2,4]))
