a16a4b35552a5fbcb6e1b6d7256d75db2439a440
Rhitu Thapa <72629071+ThapaRh@users.noreply.github.com>
2022-12-19 13:49:56 -0600

    Create ContainsDuplicate.py

---
+++ Neetcode_150/Arrays And Hashing/ContainsDuplicate.py
@@ -0,0 +1,45 @@
+"""
+
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: true
+
+"""
+
+# TC = O(n) cause we have to iterate through this
+#SC = O(n) : dictionary
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        dict={}
+        for num in nums:
+            if num in dict:
+                return True
+            else:
+                dict[num]=1
+        return False
+
+
+
+
+# this is different approach with TC: O(n) and SC: O(1) by sorting
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        nums.sort()
+        print(nums)
+        for i in range(1,len(nums)):
+            if nums[i-1]==nums[i]:
+                return True
+        return False
