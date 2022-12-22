b84dbc2047a96932054050050fe5cf772eec50c9
Rhitu Thapa <72629071+ThapaRh@users.noreply.github.com>
2022-12-22 11:44:14 -0600

    Few slidingwindow solutions

--- Neetcode_150/TwoPointers/3Sum.py
+++ Neetcode_150/TwoPointers/3Sum.py
@@ -77,3 +77,39 @@ class Solution:
                     end-=1
                 
         return count
+
+"""
+Given an integer array nums of length n and an integer target, find three integers in nums such that the sum is closest to target.
+
+Return the sum of the three integers.
+
+You may assume that each input would have exactly one solution.
+
+ 
+
+Example 1:
+
+Input: nums = [-1,2,1,-4], target = 1
+Output: 2
+Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+
+"""
+
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        count=float('inf')
+        nums.sort()
+        for i in range(len(nums)):
+            start=i+1
+            end=len(nums)-1
+            while(start<end):
+                sums=nums[i]+nums[start]+nums[end]
+                if abs(target-sums)<abs(count):
+                    count=target-sums
+                if sums>target:
+                    end-=1
+                elif sums<target:
+                    start+=1
+                else:
+                    return sums
+        return target-count
