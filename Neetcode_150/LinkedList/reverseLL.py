5ce8ffca37c97cb716b61a2ea3dd10e7109bba57
Rhitu Thapa <72629071+ThapaRh@users.noreply.github.com>
2022-12-26 17:19:15 -0600

    Create reverseLL.py

---
+++ Neetcode_150/LinkedList/reverseLL.py
@@ -0,0 +1,48 @@
+"""
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+"""
+
+#Tc=O(N) SC=O(N) for recursive
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        def rev(head,prev=None):
+            if head==None:
+               return prev
+            next=head.next
+            head.next=prev
+            return rev(next,head)
+        return rev(head,None)
+
+
+#Tc=O(N) SC=O(1)
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        curr=head
+        prev=None
+        nxt=None
+        while(curr!=None):
+            nxt=curr.next
+            curr.next=prev
+            prev=curr
+            curr=nxt
+        return prev
+
