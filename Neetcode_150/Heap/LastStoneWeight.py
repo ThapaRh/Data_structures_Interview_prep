6a368e2f2a500adc6e8561f8b41c7033ef1a36bf
Rhitu Thapa <72629071+ThapaRh@users.noreply.github.com>
2023-01-01 16:50:40 -0600

    Heapify

---
+++ Neetcode_150/Heap/LastStoneWeight.py
@@ -0,0 +1,45 @@
+"""
+You are given an array of integers stones where stones[i] is the weight of the ith stone.
+
+We are playing a game with the stones. On each turn, we choose the heaviest two stones and smash them together. Suppose the heaviest two stones have weights x and y with x <= y. The result of this smash is:
+
+If x == y, both stones are destroyed, and
+If x != y, the stone of weight x is destroyed, and the stone of weight y has new weight y - x.
+At the end of the game, there is at most one stone left.
+
+Return the weight of the last remaining stone. If there are no stones left, return 0.
+
+ 
+
+Example 1:
+
+Input: stones = [2,7,4,1,8,1]
+Output: 1
+Explanation: 
+We combine 7 and 8 to get 1 so the array converts to [2,4,1,1,1] then,
+we combine 2 and 4 to get 2 so the array converts to [2,1,1,1] then,
+we combine 2 and 1 to get 1 so the array converts to [1,1,1] then,
+we combine 1 and 1 to get 0 so the array converts to [1] then that's the value of the last stone.
+"""
+#Tc=nlog(n) SC= O(logN) for the additional input after smashing stones
+
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        heap = [-1*i for i in stones]
+        heapq.heapify(heap)
+        print(heap)
+        if len(heap)==1:
+            return heap[0]*-1
+        while(len(heap)>2):
+            h1= -1*heapq.heappop(heap)
+            h2=-1*heapq.heappop(heap)
+            val=h1-h2
+            if val==0:
+                continue
+            else:
+                heapq.heappush(heap,-val)
+        print(heap)
+        if len(heap)==1:
+            return -heap[0]
+        elif len(heap)==2:
+            return -heap[0]-(-heap[1])
